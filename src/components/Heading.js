b80ed7fbe30600a94af8edb411bae6f3228c5d25
wilsonxeem <wilsonzim566@gmail.com>
2022-07-03 01:52:16 +0100

    done

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,14 +3,13 @@ import Logo from "../secure investment data/logo.png";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-
 export default function Heading() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <Navbar sticky="top" expand="lg" style={{ backgroundColor: "white" }}>
       <Container>
         <Navbar.Brand onClick={() => navigate("/")}>
-          <img src={Logo} alt="Secure Forex Trading" width={200} />
+          <img src={Logo} alt="Secure Forex Trading" width={150} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -18,24 +17,54 @@ export default function Heading() {
             <Nav.Link onClick={() => navigate("/")}>
               <span className="li">Home</span>
             </Nav.Link>
-            <Nav.Link onClick={() => navigate("/about-us")}>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about-us");
+                window.location.reload();
+              }}
+            >
               <span className="li">About us</span>
             </Nav.Link>
-            <Nav.Link onClick={() => navigate("/account")}>
+            <Nav.Link
+              onClick={() => {
+                navigate("/account");
+                window.location.reload();
+              }}
+            >
               <span className="li">Open an Account</span>
             </Nav.Link>
-            <Nav.Link onClick={() => navigate("/performance")}>
+            <Nav.Link
+              onClick={() => {
+                navigate("/performance");
+                window.location.reload();
+              }}
+            >
               <span className="li">
                 <span className="li">Performance</span>
               </span>
             </Nav.Link>
-            <Nav.Link onClick={() => navigate("/how-it-works")}>
+            <Nav.Link
+              onClick={() => {
+                navigate("/how-it-works");
+                window.location.reload();
+              }}
+            >
               <span className="li">How it Works</span>
             </Nav.Link>
-            <Nav.Link onClick={() => navigate("/faqs")}>
+            <Nav.Link
+              onClick={() => {
+                navigate("/faqs");
+                window.location.reload();
+              }}
+            >
               <span className="li">FAQs</span>
             </Nav.Link>
-            <Nav.Link onClick={() => navigate("/contact-us")}>
+            <Nav.Link
+              onClick={() => {
+                navigate("/contact-us");
+                window.location.reload();
+              }}
+            >
               <span className="li">Contact us</span>
             </Nav.Link>
           </Nav>
