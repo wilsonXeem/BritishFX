f9d26d27659198f5202b5b563fe2950696f3eb42
wilsonxeem <wilsonzim566@gmail.com>
2022-07-03 12:17:30 +0100

    done

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -7,7 +7,7 @@ export default function ContactUs() {
       <SubHeading title="Contact Us" />
       <div className="contact">
         For opening of BritishFX account or getting information concerning
-        investment, contact us via Whatsapp at: <b>+4474058446621</b>
+        investment, contact us via Whatsapp at: <b>+447405844662</b>
         <br />
         <br />
         Or reach out to us at: <br />
@@ -15,7 +15,7 @@ export default function ContactUs() {
         <br />
         <br />
         You can click on this link to chat directly:{" "}
-        <a href="https://wa.me/4474058446621?text=%7B0%7D+Balaji+CTest">
+        <a href="https://api.whatsapp.com/send?phone=447405844662">
           <button>Chat</button>
         </a>
       </div>
